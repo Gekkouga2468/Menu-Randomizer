e3f7f896653dba872348856fe1b29ae8bc573f0a
Vinh Tran <vinhkhang186@yahoo.com>
2026-04-11 21:18:52 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <div className="card">
+        <img src="./src/assets/plusicon.png" />
+      </div>
+      <div className="card"></div>
+    </div>
+  );
+}
