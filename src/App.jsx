0ed5b01f0f9535a8a7779b28696226d4347d8a20
Vinh Tran <vinhkhang186@yahoo.com>
2026-04-12 18:17:51 -0700

    Add add card feature and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import plusIcon from "./assets/plusicon.png";
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+  const quantity = cards.length + 1;
+
+  const newCard = () => {
+    setCards((prevCards) => [...prevCards, prevCards.length + 1]);
+  };
+
   return (
-    <div>
-      <div className="card">
-        <img src="./src/assets/plusicon.png" />
+    <div className="banner">
+      <div className="slider" style={{ "--quantity": quantity }}>
+        <div className="card" onClick={newCard} style={{ "--position": 1 }}>
+          <img src={plusIcon} alt="Add card" />
+        </div>
+        {cards.map((card, index) => (
+          <div key={index} className="card" style={{ "--position": index + 2 }}>
+            Card {card}
+          </div>
+        ))}
       </div>
-      <div className="card"></div>
     </div>
   );
 }
