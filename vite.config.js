9b07f09fe9cf5e943dbce1014d5dc5d0a8697a82
Vinh Tran <vinhkhang186@yahoo.com>
2026-04-23 12:33:48 -0700

    Add deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Menu-Randomizer/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
